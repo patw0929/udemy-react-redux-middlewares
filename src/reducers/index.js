5d7f5de57e1fe722d0cc51ce63780c0dc929a8ec
patw <patw.hi@gmail.com>
2016-06-17 18:24:31 +0800

    Fixed import typo of user reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import { usersReducer } from './users';
+import usersReducer from './users';
 
 const rootReducer = combineReducers({
   users: usersReducer,
